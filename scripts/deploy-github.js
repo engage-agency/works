3805da5b90c5d42589fb0c814530bc2535617f76
engage-agency <ink_express@yahoo.com>
2021-03-14 11:01:11 +0200

    Deploy Github Pages script added

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -1,13 +1,13 @@
-// const ghpages = require('gh-pages')
+const ghpages = require('gh-pages')
 
-// // replace with your repo url
-// ghpages.publish(
-//   'public',
-//   {
-//     branch: 'public',
-//     repo: 'https://github.com/engage-agency/portfolio.git',
-//   },
-//   () => {
-//     console.log('Deploy Complete!')
-//   }
-// )
+// replace with your repo url
+ghpages.publish(
+  'public',
+  {
+    branch: 'main',
+    repo: 'https://github.com/engage-agency/works.git',
+  },
+  () => {
+    console.log('Deploy Complete!')
+  }
+)
